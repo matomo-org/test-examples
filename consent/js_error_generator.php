de7fe68f261de8e40f25330412293bd388467b4c
Zoltan Flamis <zoltan@innocraft.com>
2021-03-02 12:33:55 +1300

    Create js_error_generator.php

---
+++ consent/js_error_generator.php
@@ -0,0 +1,40 @@
+<?php include '../bootstrap.php'; ?>
+<html>
+<head>
+    <!-- Matomo -->
+    <script type="text/javascript">
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        _paq.push(['enableJSErrorTracking']);
+        (function() {
+            var u="<?php echo $matomoUrl ?>/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '<?php echo $matomoIdSite ?>']);
+
+        })();
+    </script>
+    <script type='text/javascript' src="<?php echo $matomoUrl ?>/matomo.js"></script>
+    <!-- End Matomo Code -->
+</head>
+<body>
+<h1>JS error generator</h1>
+<p>You can generate different JS error by clicking on the buttons.</p>
+<div>
+    <button onclick="jsError01()">JS error 1</button>
+    <button onclick="jsError02()">JS error 2</button>
+    <button onclick="jsError03()">JS error 3</button>
+</div>
+
+<script>
+    function jsError01() {
+        JSON('');
+    }
+
+    function jsError02() {
+        let a = b;
+    }
+</script>
+</body>
+</html>
